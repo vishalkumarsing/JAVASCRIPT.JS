5a0c4d2924cd8b5e3b144c3fb17abb242ca35650
ITS__vishal_singh <106993591+vishalkumarsing@users.noreply.github.com>
2023-05-30 13:40:16 +0530

    Create case.js

---
+++ LOOP/switch statement/case.js
@@ -0,0 +1,13 @@
+let num=5;
+switch(num){
+    case 1:console.log('v');
+    break;
+    case 2:console.log('m');
+    break;
+    case 3:console.log('h');
+    break;
+    case 4:conslole.log('m');
+    break;
+    default:console.log('k');
+    
+}
