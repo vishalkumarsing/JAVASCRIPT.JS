008b504edecca7c46d16d496156bd268ecdeb8de
ITS__vishal_singh <106993591+vishalkumarsing@users.noreply.github.com>
2023-05-31 23:03:11 +0530

    Create facory.js

---
+++ function/facory.js
@@ -0,0 +1,10 @@
+console.log('hello world');
+function createrectangle(){
+    let rectangle={
+        length:1,
+        breadth:2,
+        draw:function(){
+            console.log('draw rectangle');
+        }
+    }    
+return rectangle;
