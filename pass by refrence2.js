45acac5c379a200b6f11122bed121aab10a846c8
ITS__vishal_singh <106993591+vishalkumarsing@users.noreply.github.com>
2023-06-01 12:26:29 +0530

    Create pass by refrence2.js

---
+++ pass by refrence2.js
@@ -0,0 +1,9 @@
+let rectangle={
+    length:2,
+    breadth:3
+}
+for(let key in rectangle){
+    //key are reflected through key variable
+    //value are reflected through rectangle key
+    console.log(key,rectangle[key]);
+}
